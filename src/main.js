669d73bfddd6d3034c7e2308205a9cf7690fc967
Marcelo <marcelo.ribeiro@sysline.inf.br>
2022-08-21 12:26:42 -0300

    ajustes

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', function(req, res) {
-    res.send(`[OK] Servidor executando na porta ${port}.`);
+    res.send(`[OK] Servidor executando na porta ${port}!`);
 });
 
 app.listen(port, () => {
