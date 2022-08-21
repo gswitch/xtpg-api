91de6113b03abb9062e8df3d45d270f377e8a707
Marcelo <marcelo.ribeiro@sysline.inf.br>
2022-08-21 12:39:48 -0300

    ajustes

--- src/main.js
+++ src/main.js
@@ -1,9 +1,17 @@
-// const express = require('express');
+import compression from 'compression';
 import express from 'express';
-
-const app = express();
+import path from 'path';
+import cors from 'cors';
 
 const port = process.env.PORT || 3000;
+
+const app = express();
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
+app.use(cors({ origin: '*', methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', credentials: true }));
+app.use(express.json({ limit: '50mb', extended: true }));
+app.use(compression());
+app.use('/statics', express.static(path.resolve('./public')));
 
 app.get('/', function(req, res) {
     res.send(`[OK] Servidor executando na porta ${port}!`);
