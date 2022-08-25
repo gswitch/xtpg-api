8fad94b3a934929419569633cb072aacd10de25e
Marcelo <marcelo.ribeiro@sysline.inf.br>
2022-08-25 08:58:50 -0300

    ajustes

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import compression from 'compression';
 import express from 'express';
 import path from 'path';
+import fs from 'fs';
 import cors from 'cors';
 import { apiRoutes } from './routes.js';
 
@@ -12,8 +13,15 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors({ origin: '*', methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', credentials: true }));
 app.use(express.json({ limit: '50mb', extended: true }));
 app.use(compression());
-app.use('/statics', express.static(path.resolve('./public')));
+
+app.use(express.static(path.resolve('./public')));
 app.use('/api', apiRoutes);
+
+app.use('/*', (req, res) => {
+    let htmlFile = fs.readFileSync(path.resolve('./public/index.html'), 'utf8');
+    htmlFile = htmlFile.replace('__API__', `${req.protocol}://${req.headers.host}`);
+    res.send(htmlFile);
+});
 
 app.listen(port, () => {
     console.log(`[OK] Servidor executando na porta ${port}.`);
