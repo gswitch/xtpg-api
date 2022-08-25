572019f5a37efa8296c9e3d206fff849e12ba64b
Marcelo <marcelo.ribeiro@sysline.inf.br>
2022-08-25 09:13:22 -0300

    ajustes

--- src/main.js
+++ src/main.js
@@ -14,14 +14,23 @@ app.use(cors({ origin: '*', methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', credentia
 app.use(express.json({ limit: '50mb', extended: true }));
 app.use(compression());
 
+app.use('/', (req, res, next) => {
+    if (req.method === 'GET' && (req.originalUrl === '/' || req.originalUrl === '/index.html')) {
+        return goHome(req, res);
+    }
+    next();
+});
+
 app.use(express.static(path.resolve('./public')));
 app.use('/api', apiRoutes);
 
-app.use('/*', (req, res) => {
+app.use('/*', goHome);
+
+function goHome(req, res) {
     let htmlFile = fs.readFileSync(path.resolve('./public/index.html'), 'utf8');
     htmlFile = htmlFile.replace('__API__', `${req.protocol}://${req.headers.host}`);
     res.send(htmlFile);
-});
+}
 
 app.listen(port, () => {
     console.log(`[OK] Servidor executando na porta ${port}.`);
